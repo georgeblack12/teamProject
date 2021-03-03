90d56c3afef18a23ad8040a81799b355d3d64375
Mozhu Wang (PGT) <c0017351@newcastle.ac.uk>
2021-03-03 14:38:02 +0000

    Shopping page

---
+++ Shopping.java
@@ -0,0 +1,18 @@
+package com.project.cavallo.domain;
+
+import java.util.List;
+
+
+public class Shopping {
+
+    private List<IceCream> iceCreamList;
+
+
+    public List<IceCream> getIceCreamList() {
+        return iceCreamList;
+    }
+
+    public void setIceCreamList(List<IceCream> iceCreamList) {
+        this.iceCreamList = iceCreamList;
+    }
+}
